f1815d971ca6905554f114f59dc1f41471286158
Matej <Matej@MATEJ>
2022-01-28 11:17:31 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace HousingEstate
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+    
+    public Person()
+       {
+
+
+       }
+
+       public override string ToString()
+       {
+           return Name;
+       }
+
+
+    }
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HousingEstate
+{ 
+
+
+class Person
+    {
+    static void Main(string[] args)
+            { 
+            Person person = new Person();
+            person.Name = "Andrej";
+            Console.WriteLine(person.ToString());
+    
+    }
+    }
+}
